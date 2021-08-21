f0f433089fab0e769999289e9acb57479568ac59
mariazaikina <mashtakova93@gmail.com>
2021-08-21 18:22:53 +0400

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
 exports.min = function min (array) {
-  return 0;
+    if (Array.isArray(array) && array.length > 0){
+        if (typeof array === 'undefined'|| array.length === 0){
+            return 0;
+        }
+   let min = Math.min(...array);
+   return min;
 }
+return 0;
+}
+
 
 exports.max = function max (array) {
-  return 0;
+    if (Array.isArray(array) && array.length > 0){
+        if (typeof array === 'undefined'|| array.length === 0){
+            return 0;
+        }
+    let max = Math.max(... array);
+    return max;
+    }
+    return 0;
 }
-
-exports.avg = function avg (array) {
-  return 0;
+     
+     
+        exports.avg = function avg (array) {
+    if (Array.isArray(array) && array.length > 0){
+        if (typeof array === 'undefined'|| array.length === 0){
+            return 0;
+            }
+let avg = array.reduce((a, b) => (a + b)) / array.length;    
+return avg; 
+  
 }
+return 0;
+        }
